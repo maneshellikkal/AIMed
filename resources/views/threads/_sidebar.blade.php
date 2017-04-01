079f386ab73f7cb5ae904ca778ce98558aa2345e
Nikhil Pandey <nikhilpandey2009@gmail.com>
2017-04-01 15:45:45 +0545

    Thread Filter Improvements

--- resources/views/threads/_sidebar.blade.php
+++ resources/views/threads/_sidebar.blade.php
@@ -9,7 +9,7 @@
             Search
         </div>
         <form method="GET" action="{{ request()->url() }}" class="card-block">
-            @foreach(request()->all() as $name => $value)
+            @foreach(request()->except('search') as $name => $value)
                 <input type="hidden" name="{{ $name }}" value="{{ $value }}">
             @endforeach
             <div class="input-group">
@@ -33,28 +33,28 @@
             @if(auth()->check())
                 <li class="nav-item">
                     <a class="nav-link{{ request('author') ? ' active' : '' }}"
-                       href="{{ request()->url() }}?author={{ auth()->user()->username }}"><i class="fa fa-lightbulb-o text-info"></i> My Threads</a>
+                       href="{{ request()->url() }}?author={{ auth()->user()->username }}{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-lightbulb-o text-info"></i> My Threads</a>
                 </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('contributor') ? ' active' : '' }}"
-                   href="{{ request()->url() }}?contributor={{ auth()->user()->username }}"><i class="fa fa-code-fork text-info"></i> My Participation</a>
+                   href="{{ request()->url() }}?contributor={{ auth()->user()->username }}{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-code-fork text-info"></i> My Participation</a>
             </li>
             @endif
             <li class="nav-item">
                 <a class="nav-link{{ request('trending') ? ' active' : '' }}"
-                   href="{{ request()->url() }}?trending=1"><i class="fa fa-fire text-danger"></i> Popular This Week</a>
+                   href="{{ request()->url() }}?trending=1{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-fire text-danger"></i> Popular This Week</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('popular') ? ' active' : '' }}"
-                   href="{{ request()->url() }}?popular=1"><i class="fa fa-fire text-danger"></i> Popular All Time</a>
+                   href="{{ request()->url() }}?popular=1{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-fire text-danger"></i> Popular All Time</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('answered') == 'true' ? ' active' : '' }}"
-                   href="{{ request()->url() }}?answered=true"><i class="fa fa-thumbs-o-up text-success"></i> Answered Threads</a>
+                   href="{{ request()->url() }}?answered=true{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-thumbs-o-up text-success"></i> Answered Threads</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('answered') == 'false' ? ' active' : '' }}"
-                   href="{{ request()->url() }}?answered=false"><i class="fa fa-frown-o text-danger"></i> Unanswered Threads</a>
+                   href="{{ request()->url() }}?answered=false{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-frown-o text-danger"></i> Unanswered Threads</a>
             </li>
         </ul>
     </div>
