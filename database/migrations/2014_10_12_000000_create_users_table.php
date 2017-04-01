adfdae4a2fbc8d251b7c4fc6f8de14e0cb66833b
Nikhil Pandey <nikhilpandey2009@gmail.com>
2017-04-01 19:40:42 +0545

    Subscribe to Newsletter on Signup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('newsletter')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
