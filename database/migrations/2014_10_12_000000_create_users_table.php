ceba9b6bf6d6202a89b7468a8796290573c28c2f
Nikhil Pandey <nikhilpandey2009@gmail.com>
2017-04-02 10:41:58 +0545

    fix tests

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,14 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('occupation')->nullable();
+            $table->string('organization')->nullable();
+            $table->string('github_username')->nullable();
+            $table->string('twitter_username')->nullable();
+            $table->string('linkedin_username')->nullable();
+            $table->string('website')->nullable();
+            $table->date('dob')->nullable();
+            $table->boolean('newsletter')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
