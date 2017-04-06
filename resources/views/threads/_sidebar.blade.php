a69a60a9ea5e0a42866ef476bcd1831b69988cd8
Nikhil Pandey <nikhilpandey2009@gmail.com>
2017-04-06 08:39:18 +0545

    Fix Sidebar Bug

--- resources/views/threads/_sidebar.blade.php
+++ resources/views/threads/_sidebar.blade.php
@@ -8,7 +8,7 @@
         <div class="card-header">
             Search
         </div>
-        <form method="GET" action="{{ request()->url() }}" class="card-block">
+        <form method="GET" action="{{ request()->is('t/*') ? request()->url() : '/discuss' }}" class="card-block">
             @foreach(request()->except('search') as $name => $value)
                 <input type="hidden" name="{{ $name }}" value="{{ $value }}">
             @endforeach
@@ -28,33 +28,33 @@
         <ul class="nav stacked-tabs flex-column">
             <li class="nav-item">
                 <a class="nav-link{{ request()->url() == request()->fullUrl() ? ' active' : '' }}"
-                   href="{{ request()->url() }}"><i class="fa fa-globe text-info"></i> All Threads</a>
+                   href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}"><i class="fa fa-globe text-info"></i> All Threads</a>
             </li>
             @if(auth()->check())
                 <li class="nav-item">
                     <a class="nav-link{{ request('author') ? ' active' : '' }}"
-                       href="{{ request()->url() }}?author={{ auth()->user()->username }}{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-lightbulb-o text-info"></i> My Threads</a>
+                       href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}?author={{ auth()->user()->username }}{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-lightbulb-o text-info"></i> My Threads</a>
                 </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('contributor') ? ' active' : '' }}"
-                   href="{{ request()->url() }}?contributor={{ auth()->user()->username }}{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-code-fork text-info"></i> My Participation</a>
+                   href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}?contributor={{ auth()->user()->username }}{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-code-fork text-info"></i> My Participation</a>
             </li>
             @endif
             <li class="nav-item">
                 <a class="nav-link{{ request('trending') ? ' active' : '' }}"
-                   href="{{ request()->url() }}?trending=1{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-fire text-danger"></i> Popular This Week</a>
+                   href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}?trending=1{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-fire text-danger"></i> Popular This Week</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('popular') ? ' active' : '' }}"
-                   href="{{ request()->url() }}?popular=1{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-fire text-danger"></i> Popular All Time</a>
+                   href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}?popular=1{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-fire text-danger"></i> Popular All Time</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('answered') == 'true' ? ' active' : '' }}"
-                   href="{{ request()->url() }}?answered=true{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-thumbs-o-up text-success"></i> Answered Threads</a>
+                   href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}?answered=true{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-thumbs-o-up text-success"></i> Answered Threads</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link{{ request('answered') == 'false' ? ' active' : '' }}"
-                   href="{{ request()->url() }}?answered=false{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-frown-o text-danger"></i> Unanswered Threads</a>
+                   href="{{ request()->is('t/*') ? request()->url() : '/discuss' }}?answered=false{{ request('search') ? '&search='.request('search') : '' }}"><i class="fa fa-frown-o text-danger"></i> Unanswered Threads</a>
             </li>
         </ul>
     </div>
