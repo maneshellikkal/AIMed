a23f8241927206a362e1acc0d7c6c55800f63be1
Nikhil Pandey <nikhilpandey2009@gmail.com>
2017-04-12 21:32:59 +0545

    Fix Votes Query

--- resources/views/news/_flex_item.blade.php
+++ resources/views/news/_flex_item.blade.php
@@ -3,7 +3,7 @@
         <div>
             <div class="vote-button-container clickable d-flex flex-column" data-action="{{ $feed->path() }}/vote">
                 <div class="vote-button-caret px-2"><span class="fa fa-caret-up"></span></div>
-                <div class="vote-button-count px-2"><span>{{ $feed->votes->count() }}</span></div>
+                <div class="vote-button-count px-2"><span>{{ $feed->votes_count }}</span></div>
             </div>
         </div>
         <div class="px-2">
