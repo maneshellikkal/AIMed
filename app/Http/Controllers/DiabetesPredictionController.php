e2a9a5502bedbfd36eb6e1ad86672114730710f8
Nikhil Pandey <nikhilpandey2009@gmail.com>
2017-05-03 14:32:40 +0545

    Fix 0 bug

--- app/Http/Controllers/DiabetesPredictionController.php
+++ app/Http/Controllers/DiabetesPredictionController.php
@@ -14,16 +14,16 @@ class DiabetesPredictionController extends Controller
 
     public function predict (DiabetesPredictionRequest $request, DiabetesPredictionService $service)
     {
-        $data = $request->only([
-            'age',
-            'pregnant',
-            'plasma_glucose_concentration',
-            'diastolic_bp',
-            'tsft',
-            'serum_insulin',
-            'bmi',
-            'dpf'
-        ]);
+        $data = [
+            'age' => $request->input('age', 0),
+            'pregnant' => $request->input('pregnant', 0),
+            'plasma_glucose_concentration' => $request->input('plasma_glucose_concentration', 0),
+            'diastolic_bp' => $request->input('diastolic_bp', 0),
+            'tsft' => $request->input('tsft', 0),
+            'serum_insulin' => $request->input('serum_insulin', 0),
+            'bmi' => $request->input('bmi', 0),
+            'dpf' => $request->input('dpf', 0)
+        ];
 
         $prediction = $service->predict($data);
 
--- app/Http/Controllers/HeartDiseasePredictionController.php
+++ app/Http/Controllers/HeartDiseasePredictionController.php
@@ -14,21 +14,21 @@ class HeartDiseasePredictionController extends Controller
 
     public function predict (HeartDiseasePredictionRequest $request, HeartDiseasePredictionService $service)
     {
-        $data = $request->only([
-            'age',
-            'sex',
-            'cp',
-            'resting_bp',
-            'serum_cholesterol',
-            'fasting_blood_sugar',
-            'resting_ecg',
-            'max_heart_rate',
-            'exercise_induced_angina',
-            'st_depression',
-            'st_slope',
-            'number_of_vessels',
-            'thallium_scan_results'
-        ]);
+        $data = [
+            'age'                     => $request->input('age', 0),
+            'sex'                     => $request->input('sex', 0),
+            'cp'                      => $request->input('cp', 0),
+            'resting_bp'              => $request->input('resting_bp', 0),
+            'serum_cholesterol'       => $request->input('serum_cholesterol', 0),
+            'fasting_blood_sugar'     => $request->input('fasting_blood_sugar', 0),
+            'resting_ecg'             => $request->input('resting_ecg', 0),
+            'max_heart_rate'          => $request->input('max_heart_rate', 0),
+            'exercise_induced_angina' => $request->input('exercise_induced_angina', 0),
+            'st_depression'           => $request->input('st_depression', 0),
+            'st_slope'                => $request->input('st_slope', 0),
+            'number_of_vessels'       => $request->input('number_of_vessels', 0),
+            'thallium_scan_results'   => $request->input('thallium_scan_results', 0)
+        ];
 
         $prediction = $service->predict($data);
 
